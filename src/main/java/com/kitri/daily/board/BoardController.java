6c28172cef930335dbe9c7dddfbc50176c9bff0b
Eunjeong22 <juna9411@gmail.com>
2018-06-29 11:09:06 +0900

    파일이름 중복방지

--- src/main/java/com/kitri/daily/board/BoardController.java
+++ src/main/java/com/kitri/daily/board/BoardController.java
@@ -1,7 +1,6 @@
 package com.kitri.daily.board;
 
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
@@ -12,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
+
 
 @Controller
 public class BoardController {
@@ -31,17 +31,19 @@ public class BoardController {
 	
 	@RequestMapping(value = "/board/upload.do")
 	public String upload(HttpServletRequest req, Board b) {
-		String originPath = basePath + "\\board\\";
-		String upfolder = basePath + "\\thumbnail\\";
-		MultipartFile file = b.getFile();
+		String originPath = basePath + "\\board\\";		//원본파일 경로
+		String upfolder = basePath + "\\thumbnail\\";	//썸네일 처리한 파일 경로
+		MultipartFile file = b.getFile();				//form.jsp에서 선택한 파일 가져오기
 		if (file != null && !file.equals("")) {
 			File dir = new File(originPath);
 			if (!dir.exists()) {
 				dir.mkdirs();
 			}
-			String fileName = file.getOriginalFilename();
-			String img = fileName /*+ System.currentTimeMillis() + file.getSize()*/;
-			b.setImg(img);
+			//파일 중복방지 처리
+			String[] extension = file.getOriginalFilename().split("\\.");	
+			String FileType = extension[extension.length-1];
+			String img = b.getWriter() + "_" + System.currentTimeMillis() + "." + FileType;
+			b.setImg(img);		//img 경로 set
 			File f = new File(originPath + "\\" + img);
 			try {
 				file.transferTo(f);
@@ -51,6 +53,7 @@ public class BoardController {
 				e.printStackTrace();
 			}
 			
+			//썸네일 처리
 			int thumbnail_width = 375;
 			int thumbnail_height = 350;
 			File dir2 = new File(upfolder);
@@ -58,14 +61,13 @@ public class BoardController {
 				dir2.mkdirs();
 			}
 			try {
-				BufferedImage srcImg = ImageIO.read(f);
-				System.out.println("srcImg !! - " + srcImg);
+				BufferedImage srcImg = ImageIO.read(f);		//원본파일 읽어오기
 				BufferedImage thumbImg;
 				thumbImg = new BufferedImage(thumbnail_width, thumbnail_height, BufferedImage.TYPE_3BYTE_BGR);
 				java.awt.Graphics2D g = thumbImg.createGraphics();
 				g.drawImage(srcImg, 0, 0, thumbnail_width, thumbnail_height, null);
 				File outFile = new File(upfolder + "\\" + img);
-				ImageIO.write(thumbImg, "PNG", outFile);
+				ImageIO.write(thumbImg, "PNG", outFile);	//썸네일 파일 저장
 
 			} catch (IllegalStateException e) {
 				e.printStackTrace();
