7ad70de9d5136e8b43aa7254fdf9f6c90f7a349d
Eunjeong22 <juna9411@gmail.com>
2018-06-29 10:48:01 +0900

    썸네일 완성

--- src/main/java/com/kitri/daily/board/BoardController.java
+++ src/main/java/com/kitri/daily/board/BoardController.java
@@ -1,14 +1,16 @@
 package com.kitri.daily.board;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
 import javax.annotation.Resource;
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
@@ -16,7 +18,7 @@ public class BoardController {
 	@Resource(name = "boardService")
 	private BoardService service;
 	
-	public static String basePath = "D:\\Driver\\apache-tomcat-8.5.30\\webapps\\board\\";
+	public static String basePath = "D:\\Driver\\apache-tomcat-8.5.30\\webapps\\";
 
 	public void setService(BoardService service) {
 		this.service = service;
@@ -29,20 +31,42 @@ public class BoardController {
 	
 	@RequestMapping(value = "/board/upload.do")
 	public String upload(HttpServletRequest req, Board b) {
+		String originPath = basePath + "\\board\\";
+		String upfolder = basePath + "\\thumbnail\\";
 		MultipartFile file = b.getFile();
 		if (file != null && !file.equals("")) {
-			File dir = new File(basePath);
+			File dir = new File(originPath);
 			if (!dir.exists()) {
 				dir.mkdirs();
 			}
-			int thumbnail_width = 300;
-			int thumbnail_height = 300;
 			String fileName = file.getOriginalFilename();
 			String img = fileName /*+ System.currentTimeMillis() + file.getSize()*/;
-			File f = new File(basePath + "\\" + img);
 			b.setImg(img);
+			File f = new File(originPath + "\\" + img);
 			try {
 				file.transferTo(f);
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			int thumbnail_width = 375;
+			int thumbnail_height = 350;
+			File dir2 = new File(upfolder);
+			if (!dir2.exists()) {
+				dir2.mkdirs();
+			}
+			try {
+				BufferedImage srcImg = ImageIO.read(f);
+				System.out.println("srcImg !! - " + srcImg);
+				BufferedImage thumbImg;
+				thumbImg = new BufferedImage(thumbnail_width, thumbnail_height, BufferedImage.TYPE_3BYTE_BGR);
+				java.awt.Graphics2D g = thumbImg.createGraphics();
+				g.drawImage(srcImg, 0, 0, thumbnail_width, thumbnail_height, null);
+				File outFile = new File(upfolder + "\\" + img);
+				ImageIO.write(thumbImg, "PNG", outFile);
+
 			} catch (IllegalStateException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
