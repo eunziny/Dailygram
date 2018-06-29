2e4ee9456f93f6092f8b5185f6f0ae30e1a76af8
zzaenni515 <zzaenni515@naver.com>
2018-06-29 10:17:59 +0900

    친구 추천 조건1 완성

--- src/main/java/com/kitri/daily/friend/FriController.java
+++ src/main/java/com/kitri/daily/friend/FriController.java
@@ -30,15 +30,21 @@ public class FriController {
 		//로그인한 사람의 친구 수 구하기
 		//ArrayList<Integer> count = (ArrayList<Integer>) service.getFriendRelationshipCount(user_id);
 		List<HashMap<String, Object>> count = service.getFriendRelationshipCount(user_id);
-		System.out.println("count의 크기 : " + count.size() + ", 0번째 : " + count.get(0));
-		if(count.size()==1 && count.get(0).equals("0")) {//친구 수가 0이고 좋아요 한 글이 없다면 최신글순으로 좋아요 많이 받은 회원 추천하기
+		//System.out.println("count의 크기 : " + count.size() + ", 0번째 : " + count.get(0).get("cnt")+ ", " + count.get(1).get("cnt"));
+		System.out.println("count의 크기 : " + count.size() + ", 0번째 : " + count.get(0).get("cnt"));
+		if(count.size()==1) {//친구 수가 0이고 좋아요 한 글이 없다면 최신글순으로 좋아요 많이 받은 회원 추천하기
+			System.out.println("친구 0명");
 			ArrayList<Friend> list = (ArrayList<Friend>) service.getRecommend();
 			mav.addObject("list", list);
 			return mav;
-		}//else{//친구 수가 1명 이상 있으면
+		}else{//친구 수가 1명 이상 있으면
+			System.out.println("친구 여러명");
 			// intro 해시태그 기준으로 회원 추천해주기
 			
-		//}
-		return null;
+			System.out.println("조건 만족X");	
+			
+			return null;
+		}
+		
 	}
 }
